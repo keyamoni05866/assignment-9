f2c342d5af172c50b4b6ec1870c99de39a421c63
keyamoni05866 <keya05866@gmail.com>
2023-04-10 02:38:24 +0600

    navbar added

---
+++ src/Components/AppliedJobs/Jobs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Jobs = () => {
+    return (
+        <div>
+            <h2>this is job</h2>
+        </div>
+    );
+};
+
+export default Jobs;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Components/Home/Home';
+import Main from './Components/Layout/Main';
+import Statistics from './Components/Statistics/Statistics';
+import Jobs from './Components/AppliedJobs/Jobs';
+import Blog from './Components/Blog/Blog';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: 'statistics',
+        element: <Statistics></Statistics>
+      },
+      {
+        path: 'jobs',
+        element: <Jobs></Jobs>
+      },
+      {
+        path: 'blog',
+        element: <Blog></Blog>
+      }
+    ]
+
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
