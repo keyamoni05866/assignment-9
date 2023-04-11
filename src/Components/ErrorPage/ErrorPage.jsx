471b083e9e47d5684b03c4fa763944e6128fb11a
keyamoni05866 <keya05866@gmail.com>
2023-04-12 02:26:57 +0600

    error page modified

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
-        <div className='flex justify-center mt-64 '>
+        <div  className='grid col-span-1 justify-center place-items-center  mt-36'>
                     
-                 <h1 className=' text-7xl text-cyan-700'>4O4</h1>
-                 
+                <div>
+                <h1 className=' text-9xl text-cyan-700'>404</h1>
+                </div>
+             <div>
+             <p className='text-4xl font-semibold mt-4 text-center'>Ooooppsss !!</p>
+                 <h1 className='text-4xl font-semibold mt-4 '>You are in the wrong page</h1>
+                 <button className=" ms-36 mt-7 py-4 px-8 apply text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 ">
+        <Link to="/">    Go Home</Link>
+          </button>
+             </div>
         </div>
     );
 };
