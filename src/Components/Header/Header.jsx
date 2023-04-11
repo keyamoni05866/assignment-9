247ff969f0b3ad4f7246a9795037cef9af568133
keyamoni05866 <keya05866@gmail.com>
2023-04-11 21:34:15 +0600

    chart added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -35,7 +35,7 @@ const Header = () => {
               <Link to="/blog">Blog</Link>
             </ul>
           </div>
-          <a className="btn btn-ghost normal-case text-2xl">JobHunter</a>
+          <a className="btn btn-ghost normal-case text-2xl">JobStore</a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -1,11 +1,78 @@
-import React from 'react';
+import React from "react";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 const Statistics = () => {
-    return (
-        <div>
-            <h2>this is Statistics</h2>
-        </div>
-    );
+  const Marks = [
+    {
+      name: "Assignment-1",
+    //   uv: 0,
+      marks: 60,
+    },
+    {
+      name: "Assignment-2",
+    //   uv: 10,
+      marks: 55,
+    },
+    {
+      name: "Assignment-3",
+    //   uv: 20,
+      marks: 58,
+    },
+    {
+      name: "Assignment-4",
+    //   uv: 30,
+      marks: 54,
+    },
+    {
+      name: "Assignment-5",
+    //   uv: 40,
+      marks: 57,
+    },
+    {
+      name: "Assignment-6",
+    //   uv: 50,
+      marks: 55,
+    },
+    {
+      name: "Assignment-7",
+    //   pv: 60,
+      marks: 60,
+    },
+  ];
+
+  return (
+    <div>
+      <h2 className="text-2xl text-center font-semibold mb-5 underline">Charts of the Assignment Marks</h2>
+
+      <ResponsiveContainer width="100%" aspect={3}>
+        <AreaChart
+          width={500}
+          height={400}
+          data={Marks}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Area type="monotone" dataKey="marks" stroke="#8884d8" fill="#8884d8" />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 export default Statistics;
